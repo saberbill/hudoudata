0e9be0de5ef365c3042e4498d8ec782dcceaa7f4
Personal <fku@gmail.com>
2018-01-30 11:34:18 +0800

    updated.

--- welcome/views.py
+++ welcome/views.py
@@ -9,14 +9,23 @@ from .models import PageView
 # Create your views here.
 
 def index(request):
-    hostname = os.getenv('HOSTNAME', 'unknown')
-    PageView.objects.create(hostname=hostname)
+    #hostname = os.getenv('HOSTNAME', 'unknown')
+    #PageView.objects.create(hostname=hostname)
 
+    return render(request, 'index.html',{
+        'amount': 1807.47,
+        'totalRooms': 65,
+        'soldRooms': 13,
+        'rentPercentage': '21%',
+    })
+    '''
     return render(request, 'welcome/index.html', {
         'hostname': hostname,
         'database': database.info(),
         'count': PageView.objects.count()
     })
+    '''
 
 def health(request):
-    return HttpResponse(PageView.objects.count())
+    return render(request, 'index.html')
+    #return HttpResponse(PageView.objects.count())
