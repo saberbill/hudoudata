0da025cf3a7a261bc0b213b98b513f6ba430d25e
Personal <fku@gmail.com>
2018-02-01 12:20:55 +0800

    updated.

--- project/database.py
+++ project/database.py
@@ -12,10 +12,13 @@ engines = {
 
 def config():
     service_name = os.getenv('DATABASE_SERVICE_NAME', 'postgresql').upper().replace('-', '_')
+    '''
     if service_name:
         engine = engines.get(os.getenv('DATABASE_ENGINE'), engines['sqlite'])
     else:
         engine = engines['sqlite']
+    '''
+    engine = engines['sqlite']
     name = os.getenv('DATABASE_NAME')
     if not name and engine == engines['sqlite']:
         name = os.path.join(settings.BASE_DIR, 'db.sqlite3')
