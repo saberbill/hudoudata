aa7575492a32c26dbdedb7473f5bf4918c7eb4ea
jeffjiang <chuanchuanj@gmail.com>
2018-02-01 19:45:01 +0800

    UPdated.

--- hudou/migrations/0001_initial.py
+++ hudou/migrations/0001_initial.py
@@ -10,71 +10,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-
-        migrations.CreateModel(
-            name='Area',
-            fields=[
-                ('id', models.AutoField(primary_key=True, serialize=False)),
-                ('lid', models.CharField(max_length=40)),
-                ('areaName', models.CharField(db_column='area_name', max_length=30)),
-                ('long', models.FloatField()),
-                ('lat', models.FloatField()),
-            ],
-            options={
-                'db_table': 'area',
-            },
-        ),
-        migrations.CreateModel(
-            name='DailySummary',
-            fields=[
-                ('id', models.AutoField(primary_key=True, serialize=False)),
-                ('totalRooms', models.IntegerField(db_column='total_rooms')),
-                ('soldRooms', models.IntegerField(db_column='sold_rooms')),
-                ('turnover', models.FloatField()),
-                ('date', models.DateField()),
-                ('lastUpdated', models.DateTimeField(db_column='last_updated')),
-            ],
-            options={
-                'db_table': 'daily_summary',
-            },
-        ),
-        migrations.CreateModel(
-            name='House',
-            fields=[
-                ('id', models.AutoField(primary_key=True, serialize=False)),
-                ('uuid', models.CharField(max_length=40)),
-                ('areaId', models.IntegerField(db_column='area_id')),
-                ('areaName', models.CharField(db_column='area_name', max_length=30)),
-                ('lid', models.CharField(max_length=40)),
-                ('title', models.CharField(max_length=128)),
-                ('price', models.FloatField()),
-                ('lang', models.FloatField()),
-                ('lat', models.FloatField()),
-                ('model', models.CharField(max_length=30)),
-                ('status', models.SmallIntegerField()),
-                ('lastUpdated', models.DateTimeField(db_column='last_updated')),
-                ('createTime', models.DateTimeField(db_column='create_time')),
-            ],
-            options={
-                'db_table': 'house',
-            },
-        ),
-        migrations.CreateModel(
-            name='HouseSold',
-            fields=[
-                ('id', models.AutoField(primary_key=True, serialize=False)),
-                ('houseId', models.IntegerField(db_column='house_id')),
-                ('price', models.FloatField()),
-                ('specialPrice', models.FloatField(db_column='special_price')),
-                ('status', models.SmallIntegerField()),
-                ('date', models.DateField()),
-                ('lastUpdated', models.DateTimeField(db_column='last_updated')),
-            ],
-            options={
-                'db_table': 'house_sold',
-            },
-        ),
-        '''
         migrations.CreateModel(
             name='PageView',
             fields=[
@@ -84,6 +19,70 @@ class Migration(migrations.Migration):
             ],
         ),
         '''
+            migrations.CreateModel(
+                name='Area',
+                fields=[
+                    ('id', models.AutoField(primary_key=True, serialize=False)),
+                    ('lid', models.CharField(max_length=40)),
+                    ('areaName', models.CharField(db_column='area_name', max_length=30)),
+                    ('long', models.FloatField()),
+                    ('lat', models.FloatField()),
+                ],
+                options={
+                    'db_table': 'area',
+                },
+            ),
+            migrations.CreateModel(
+                name='DailySummary',
+                fields=[
+                    ('id', models.AutoField(primary_key=True, serialize=False)),
+                    ('totalRooms', models.IntegerField(db_column='total_rooms')),
+                    ('soldRooms', models.IntegerField(db_column='sold_rooms')),
+                    ('turnover', models.FloatField()),
+                    ('date', models.DateField()),
+                    ('lastUpdated', models.DateTimeField(db_column='last_updated')),
+                ],
+                options={
+                    'db_table': 'daily_summary',
+                },
+            ),
+            migrations.CreateModel(
+                name='House',
+                fields=[
+                    ('id', models.AutoField(primary_key=True, serialize=False)),
+                    ('uuid', models.CharField(max_length=40)),
+                    ('areaId', models.IntegerField(db_column='area_id')),
+                    ('areaName', models.CharField(db_column='area_name', max_length=30)),
+                    ('lid', models.CharField(max_length=40)),
+                    ('title', models.CharField(max_length=128)),
+                    ('price', models.FloatField()),
+                    ('lang', models.FloatField()),
+                    ('lat', models.FloatField()),
+                    ('model', models.CharField(max_length=30)),
+                    ('status', models.SmallIntegerField()),
+                    ('lastUpdated', models.DateTimeField(db_column='last_updated')),
+                    ('createTime', models.DateTimeField(db_column='create_time')),
+                ],
+                options={
+                    'db_table': 'house',
+                },
+            ),
+            migrations.CreateModel(
+                name='HouseSold',
+                fields=[
+                    ('id', models.AutoField(primary_key=True, serialize=False)),
+                    ('houseId', models.IntegerField(db_column='house_id')),
+                    ('price', models.FloatField()),
+                    ('specialPrice', models.FloatField(db_column='special_price')),
+                    ('status', models.SmallIntegerField()),
+                    ('date', models.DateField()),
+                    ('lastUpdated', models.DateTimeField(db_column='last_updated')),
+                ],
+                options={
+                    'db_table': 'house_sold',
+                },
+            ),
+            '''
     ]
 
 
