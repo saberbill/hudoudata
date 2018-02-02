d71049a9616e4bfcac05162594a92665bdd2ab05
Personal <fku@gmail.com>
2018-02-02 17:08:00 +0800

    updated.

--- project/urls.py
+++ project/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from hudou.views import index, health, getReportSummary, getLatestSummary, getHouseArea
+from hudou.views import index, health, getLatestSummary, getHouseArea
 
 urlpatterns = [
     # Examples:
